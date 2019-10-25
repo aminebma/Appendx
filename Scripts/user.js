73df3e0fd98d215b0ec139c4207723ed33e1f869
Mohamed Abdelmadjid Boudis <34186054+badido18@users.noreply.github.com>
2019-10-25 08:32:08 +0100

    init

---
+++ Scripts/user.js
@@ -0,0 +1,14 @@
+class user{
+    static genID;
+   constructor(Name,age,pays,ville){
+       this.id = ++genID;
+       this.Name = Name;
+       this.age = age;
+       this.pays = pays;
+       this.ville = ville;
+   };
+
+
+   
+
+}
